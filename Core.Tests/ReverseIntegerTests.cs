4698603b5c2e9d1bc693014ac99ce9ba8f2b1980
Harlach, Michael <mharlach@gpstrategies.com>
2019-12-10 09:49:35 -0500

    updates

--- Core.Tests/ReverseIntegerTests.cs
+++ Core.Tests/ReverseIntegerTests.cs
@@ -8,29 +8,52 @@ namespace Core.Tests
         [TestMethod]
         public void Case1()
         {
-            var r = new ReverseInteger().Go(123);
+            var r = new ReverseInteger().Go2(123);
             Assert.AreEqual(321, r);
         }
 
         [TestMethod]
         public void Case2()
         {
-            var r = new ReverseInteger().Go(-123);
+            var r = new ReverseInteger().Go2(-123);
             Assert.AreEqual(-321, r);
         }
 
         [TestMethod]
         public void Case3()
         {
-            var r = new ReverseInteger().Go(120);
+            var r = new ReverseInteger().Go2(120);
             Assert.AreEqual(21, r);
         }
 
         [TestMethod]
         public void Case4()
         {
-            var r = new ReverseInteger().Go(0);
+            var r = new ReverseInteger().Go2(0);
             Assert.AreEqual(0, r);
         }
+
+        [TestMethod]
+        public void Case6()
+        {
+            var r = new ReverseInteger().Go2(12345);
+            Assert.AreEqual(54321, r);
+        }
+
+        [TestMethod]
+        public void Case5()
+        {
+            var r = new ReverseInteger().Go2(1534236469);
+            //Assert.AreEqual(9646324351, r);
+            Assert.AreEqual(0, r);
+        }
+
+        [TestMethod]
+        public void Case7()
+        {
+            var r = new ReverseInteger().Go2(1563847412);
+            Assert.AreEqual(0, r);
+        }
+
     }
 }
--- Core/ReverseInteger.cs
+++ Core/ReverseInteger.cs
@@ -12,14 +12,29 @@ namespace Core
             {
                 bool negative = x > -1 ? false : true;
                 x = Math.Abs(x);
+                var resultQueue = new Queue<int>();
                 do
                 {
-                    var pow = Convert.ToInt32(Math.Floor(Math.Log10(x)));
-                    var tmp = Convert.ToInt32((Math.Pow(10, pow))) * (x % 10);
-                    result += tmp;
-                    x = x / 10;
+                    var a = x / 10;
+                    var b = a * 10;
+                    var diff = x - b;
 
+                    resultQueue.Enqueue(diff);
+                    x = a;
                 } while (x > 0);
+
+                do
+                {
+                    var count = resultQueue.Count;
+                    var pow = Math.Pow(10, count - 1);
+                    var v = pow * resultQueue.Dequeue();
+                    if (v > (double)int.MaxValue)
+                    {
+                        return 0;
+                    }
+                    
+                    result += (int)v;
+                } while (resultQueue.Count > 0);
 
                 if (negative)
                 {
@@ -29,6 +44,37 @@ namespace Core
             return result;
         }
 
+        public int Go2(int x)
+        {
+            int result = 0;
+            if (x != 0 && x > int.MinValue && x < int.MaxValue)
+            {
+                bool negative = x > -1 ? false : true;
+                x = Math.Abs(x);
+                do
+                {
+                    var a = x / 10;
+                    var b = a * 10;
+                    var diff = x - b;
+                    var digits = Math.Floor(Math.Log10(x) + 1);
+                    var add = Math.Pow(10,digits-1) * diff;
+                    if(add > (double)int.MaxValue || add+(double)result >(double)int.MaxValue)
+                    {
+                        return 0;
+                    }
+
+                    result += (int)add;
+                    x = x/10;
+
+                }while(x >0);
+
+                if (negative)
+                {
+                    result = -1 * result;
+                }
+            }
+            return result;
+        }
 
     }
 }
